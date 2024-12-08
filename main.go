ee3bce3826b1bb73cce6816fb8dc1960dadbeaea
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-08 17:50:11 +0530

    Created two talkers, each saying their own phrase
    Created two talkers, each saying their own phrase

--- main.go
+++ main.go
@@ -9,14 +9,15 @@ import (
 
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func say(phrase string) {
+func say(id int, phrase string) {
 	for _, word := range strings.Fields(phrase) {
-		fmt.Printf("Abhijeet says %s...\n", word)
+		fmt.Printf("Worker #%d says %s...\n", id, word)
 		dur := time.Duration(rand.Intn(100)) * time.Millisecond
 		time.Sleep(dur)
 	}
 }
 
 func main() {
-	say("Golang is awesome")
+	say(1, "Golang is awesome")
+	say(2, "Rust is faster")
 }
