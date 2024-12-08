9b325034f8b59174c9b27674f127880edfadaebb
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-08 18:01:09 +0530

    Separate concurrency logic from business logic
    Separate concurrency logic from business logic

--- main.go
+++ main.go
@@ -10,20 +10,24 @@ import (
 
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func say(wg *sync.WaitGroup, id int, phrase string) {
+func say(id int, phrase string) {
 	for _, word := range strings.Fields(phrase) {
 		fmt.Printf("Worker #%d says %s...\n", id, word)
 		dur := time.Duration(rand.Intn(100)) * time.Millisecond
 		time.Sleep(dur)
 	}
-	wg.Done()
 }
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go say(&wg, 1, "Golang is awesome")
-	wg.Add(1)
-	go say(&wg, 2, "Cats are cute")
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		say(1, "Golang is awesome")
+	}()
+	go func() {
+		defer wg.Done()
+		say(2, "Cats are cute")
+	}()
 	wg.Wait()
 }
