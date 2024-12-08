7ad14e50a8d1745ebffcd9232f3e55360560c233
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-08 17:58:29 +0530

    Add waitGroup
    Add waitGroup

--- main.go
+++ main.go
@@ -4,21 +4,26 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+	"sync"
 	"time"
 )
 
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func say(id int, phrase string) {
+func say(wg *sync.WaitGroup, id int, phrase string) {
 	for _, word := range strings.Fields(phrase) {
 		fmt.Printf("Worker #%d says %s...\n", id, word)
 		dur := time.Duration(rand.Intn(100)) * time.Millisecond
 		time.Sleep(dur)
 	}
+	wg.Done()
 }
 
 func main() {
-	go say(1, "Golang is awesome")
-	go say(2, "Cats are cute")
-	time.Sleep(time.Second * 5)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go say(&wg, 1, "Golang is awesome")
+	wg.Add(1)
+	go say(&wg, 2, "Cats are cute")
+	wg.Wait()
 }
