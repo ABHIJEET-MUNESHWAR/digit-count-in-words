35a731667fc45c02ca8c29994d3c55af187ee8b8
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-08 17:47:30 +0530

    🗣️ speak a phrase word by word with some pauses
    🗣️ speak a phrase word by word with some pauses

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+func say(phrase string) {
+	for _, word := range strings.Fields(phrase) {
+		fmt.Printf("Abhijeet says %s...\n", word)
+		dur := time.Duration(rand.Intn(100)) * time.Millisecond
+		time.Sleep(dur)
+	}
+}
 
 func main() {
+	say("Golang is awesome")
 }
